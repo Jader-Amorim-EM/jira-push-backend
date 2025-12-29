faf726c7da11f2876d025aa2eb4c6c22e61b0b72
QA_JaderAmorim <jader.amorim@escolarmanager.com.br>
2025-12-29 14:13:58 -0300

    fix: Ajuste apresentação mais clara histórico 8

--- src/routes/jira.routes.js
+++ src/routes/jira.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { sendNotification } from "../services/push.service.js";
+import { sendNotification, initWebPush } from "../services/push.service.js";
 
 const router = express.Router();
 
